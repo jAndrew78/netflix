35d4c4216492f6a128c06936d54fa40fc891c88c
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-16 14:21:27 -0500

    renamed accordion.js to faqs.js in containers and started fleshing it out, updated import in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FooterContainer from './containers/footer';
 import JumbotronContainer from './containers/jumbotron';
-import AccordionContainer from './containers/accordion';
+import AccordionContainer from './containers/faqs';
 
 
 export default function App() {
---
+++ src/containers/faqs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import faqsData from '../fixtures/faqs.json';
+import { Accordion } from '../components';
+
+
+export default function FaqsContainer() {
+    return (
+        <Accordion>
+            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+            <Accordion.Item>
+
+            </Accordion.Item>
+        </Accordion>
+    );
+}
