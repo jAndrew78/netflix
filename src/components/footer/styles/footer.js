51c16bdab6e6b9802e3a9f75fef8bbaac81cdf6c
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-16 17:50:14 -0500

    fixed footer center / grid spacing

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -2,23 +2,48 @@ import styled from 'styled-components/macro';
 
 export const Container = styled.div`
     display: flex;
-    padding: 70px 56px;
+    padding: 70px 0 70px 90px;
     margin: auto;
     max-width: 1000px;
     flex-direction: column;
 
     @media (max-width: 1000px) {
-        padding: 70px 30px;
+        padding: 70px 0 70px 90px;
     }
+    
+    @media (max-width: 850px) {
+        padding: 70px 0 70px 70px;
+    }
+
+    @media (max-width: 715px) {
+        padding: 70px 0 70px 30px;
+    }
+   
+    @media (max-width: 600px) {
+        padding: 70px 0 70px 80px;
+    }
+
 `;
 
 export const Row = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(205px, 1fr));  // or try 210
+    grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));  // or try 210
     grid-gap: 15px;
 
     @media (max-width: 1000px) {
-        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+        grid-template-columns: repeat(auto-fill, minmax(170px, 1fr));
+    }
+
+    @media (max-width: 850px) {
+        grid-template-columns: repeat(auto-fill, minmax(145px, 1fr));
+    }
+
+    @media (max-width: 715px) {
+        grid-template-columns: repeat(auto-fill, minmax(125px, 1fr));
+    }
+
+    @media (max-width: 600px) {
+        grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
     }
 `;
 
