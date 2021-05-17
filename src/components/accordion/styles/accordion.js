548a8e979a857792624e378627e38e5cae704c8f
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-17 15:13:11 -0500

    fixed spacing and responsiveness

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -22,6 +22,8 @@ export const Item = styled.div`
     color: white;
     margin-bottom: 10px;
     max-width: 670px;
+    width: 100%;
+    margin: auto;
 
     &:first-of-type {
         margin-top: 3em;
@@ -34,6 +36,7 @@ export const Body = styled.div`
     font-size: 26px;
     font-weight: normal;
     line-height: normal;
+    margin-bottom: 2px;
     background: #303030;
     padding: 0.8em 2.2em;
     white-space: pre-wrap;
@@ -49,7 +52,7 @@ export const Header = styled.div`
     display: flex;
     justify-content: space-between;
     cursor: pointer;
-    margin-bottom: 1px;
+    margin: 2px 0;
     font-size: 26px;
     font-weight: normal;
     background: #303030;
