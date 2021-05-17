4c5bd04ee6fdb71085c9995e904b53599cfbde21
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-17 13:42:57 -0500

    added img to button

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -11,7 +11,12 @@ OptForm.Input = function OptFormInput({ ...restProps }) {
 }
 
 OptForm.Button = function OptFormButton({ children, ...restProps }) {
-    return <Button {...restProps}>{ children }</Button>;
+    return (
+        <Button {...restProps}>
+            { children }
+            <img src="/images/icons/chevron-right.png" alt="Try Now" />
+        </Button>
+    ); 
 }
 
 OptForm.Text = function OptFormText({ children, ...restProps }) {
