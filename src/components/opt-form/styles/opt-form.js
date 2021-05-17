513e97761f6188591422a0111ee8efe130977f76
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-17 14:33:09 -0500

    styled container, input and button

--- src/components/opt-form/styles/opt-form.js
+++ src/components/opt-form/styles/opt-form.js
@@ -1,9 +1,48 @@
 import styled from 'styled-components/macro';       // adding macro gives us name of component in browser dev tools 
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    height: 100%;
+    margin-top: 20px;
+    flex-wrap: wrap;
 
-export const Input = styled.div``;
+    @media (max-width 1000px) {
+        flex-direction: column;
+        align-items: center;
+    }
+`;
 
-export const Button = styled.div``;
+export const Input = styled.input`
+    max-width: 450px;
+    width: 100%;
+    border: 0;
+    padding: 10px;
+    height: 70px;
+    box-sizing: border-box;
+`;
+
+export const Button = styled.button`
+    display: flex;
+    align-items: center;
+    height: 70px;
+    background: #e50914;
+    color: white;
+    text-transform: uppercase;
+    padding: 0 32px;
+    font-size: 26px;
+    border: 0;
+    cursor: pointer;
+
+    img {
+        margin-left: 10px;
+        filter: brightness(0) invert(1);
+        width: 24px;
+
+        @media (max-width: 1000px) {
+            width: 16px;
+        }
+    }
+`;
 
 export const Text = styled.p``;
