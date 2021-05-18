b7cf907be0866418cd1a89eba440a17f7e875049
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-17 20:36:42 -0500

    added background image to header, re-using  opt form in header on pages/home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,12 +4,23 @@ import HeaderContainer from '../containers/header';
 import JumbotronContainer from '../containers/jumbotron';
 import FaqsContainer from '../containers/faqs';
 import FooterContainer from '../containers/footer';
+import { OptForm } from '../components';
 
 
 export default function Home() {
     return (
         <>
-            <HeaderContainer />
+            <HeaderContainer>
+                <OptForm>
+                    <OptForm.Text>
+                        Ready to watch? Enter your email to create or restart your membership.
+                    </OptForm.Text>
+                    <OptForm.Break />
+                    <OptForm.Input placeholder="Email address" />
+                    <OptForm.Button>Get Started</OptForm.Button>
+                </OptForm>
+            </HeaderContainer>
+
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />        
