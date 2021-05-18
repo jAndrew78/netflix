cbcb4dc1d4e8dd765b595c14113f16b6c00b2fb9
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-17 21:23:01 -0500

    fixed feature styling

--- src/components/feature/styles/feature.js
+++ src/components/feature/styles/feature.js
@@ -1,8 +1,33 @@
 import styled from 'styled-components/macro';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-bottom: 8px solid #222;
+    text-align: center;
+    padding: 125px 45px 150px 45px;
+`;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+    color: white;
+    max-width: 700px;
+    font-size: 65px;
+    font-weight: 600;
+    margin: auto;
 
-export const SubTitle = styled.h2``;
+    @media (max-width: 800px) {
+        font-size: 35px;
+    }
+`;
+
+export const SubTitle = styled.h2`
+    color: white;
+    font-size: 26px;
+    font-weight: normal;
+    margin: 16px auto;
+
+    @media (max-width: 800px) {
+        font-size: 18px;
+    }
+`;
 
