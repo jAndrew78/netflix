aab42277ebb9b995422f51ce0b5edcc9e924ede2
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-21 16:09:55 -0500

    firebase logic on sign up page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -23,6 +23,24 @@ export default function Signup() {
         event.preventDefault();
 
         //firebase
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(emailAddress, password)
+            .then((result) =>
+                result.user.updateProfile({
+                    displayName: firstName,
+                    photoURL: Math.floor(Math.random() * 5) + 1,
+                })
+                .then(() => {
+                    history.push(ROUTES.BROWSE);
+                })
+            )
+            .catch(error => {
+                setFirstName('');
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            })
     }
 
     return (
