5912c61f7a89bf21f6ddd7df771f98db643f2c09
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-22 15:05:11 -0500

    set up useState for profile and loading, useEffect to test loading, passed setProfile through SelectProfileContainer

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,11 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
 
 
 export function BrowseContainer({ slides }) {
+    const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
     const { firebase } = useContext(FirebaseContext);
     const user = firebase.auth().currentUser || {};
 
-    return <SelectProfileContainer user={user} />;
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, [profile.displayName])
+
+    return <SelectProfileContainer user={user} setProfile={setProfile} />;
 }
