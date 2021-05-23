70b973bc6a036ece7a2dd36071a1b49d5782cba1
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-22 20:04:54 -0500

    loading screen set up and styled

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { Container } from './styles/loading';
+import { Spinner, LockBody, ReleaseBody, Picture } from './styles/loading';
 
 
-export default function Loading({ children, ...restProps }) {
-    return <Container {...restProps}>{children}</Container>
+export default function Loading({ src, ...restProps }) {
+    return (
+        <Spinner {...restProps}>
+            <LockBody />
+            <Picture src={`/images/users/${src}.png`} />
+        </Spinner>
+    )
+}
+
+Loading.ReleaseBody = function LoadingReleaseBody() {
+    return <ReleaseBody />
 }
