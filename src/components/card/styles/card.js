3c7dade0a09cced3916e89e71e7653fce347e488
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-05-23 21:18:50 -0500

    Card styling

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -1,15 +1,42 @@
 import styled from 'styled-components/macro';
 
-export const Container = styled.div`
+export const Title = styled.p`
+    font-size: 24px;
+    color: #e5e5e5;
+    font-weight: bold;
+    margin-left: 56px;
+    margin-right: 56px;
+    margin-top: 0;
+`;
 
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 50px;
+    box-sizing: border-box;
+
+    > ${Title} {
+        @media (max-width: 1000px) {
+            margin-left: 30px;
+        }
+    }
+
+    &:last-of-type {
+        margin-bottom: 0;
+    }
 `;
 
 export const Group = styled.div`
+    display: flex;
+    flex-direction: ${({ flexDirection }) => flexDirection === 'row' ? 'row' : 'column'};
+    ${({ alignItems }) => alignItems && `align-items: ${alignItems}`}
+    ${({ margin }) => margin && `margin: ${margin}`}
 
-`;
-
-export const Title = styled.p`
-
+    >${Container}:first-of-type {
+        @media (min-width: 1100px) {
+            margin-top: -150px;
+        }
+    }
 `;
 
 export const SubTitle = styled.p`
@@ -25,7 +52,7 @@ export const Feature = styled.div`
 `;
 
 export const FeatureTitle = styled(Title)`
-
+    margin-left: 0;
 `;
 
 export const FeatureText = styled.p`
@@ -33,23 +60,55 @@ export const FeatureText = styled.p`
 `;
 
 export const FeatureClose = styled.button`
+    color: #fff;
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    cursor: pointer;
+    background-color: transparent;
+    border: 0;
 
+    img (
+        filter: brightness(0) invert(1);
+        width: 24px;
+    )
 `;
 
 export const Maturity = styled.div`
-
+    background-color: ${({ rating }) => (rating >= 15 ? 'red' : 'green')};
+    border-radius: 15px;
+    width: 20px;
+    padding: 5px;
+    text-align: center;
+    color: #fff;
+    font-weight: bold;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+    margin-right: 10px;
+    font-size: 12px;
 `;
 
 export const Content = styled.div`
+    margin: 56px;
+    max-width: 500px;
+    line-height: normal;
 
+    @media (max-width: 1000px) {
+        margin: 30px;
+        max-width: none;
+    }
 `;
 
 export const Entities = styled.div`
-
+    display: flex;
+    flex-direction: row;
 `;
 
 export const Meta = styled.div`
-
+    display: none;
+    position: absolute;
+    bottom: 0;
+    padding: 10px;
+    background-color: #0000008f;
 `;
 
 export const Item = styled.div`
@@ -57,5 +116,11 @@ export const Item = styled.div`
 `;
 
 export const Image = styled.img`
-
+    border: 0;
+    height: auto;
+    width: 100%;
+    max-width: 305px;
+    cursor: pointer;
+    padding: 0;
+    margin: 0;
 `;
