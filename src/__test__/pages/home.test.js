7141fee240171a3c2fec74d6dec2fd8d88c6e9f9
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-06-24 11:58:49 -0500

    removed unused import

--- src/__test__/pages/home.test.js
+++ src/__test__/pages/home.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Home } from '../../pages';
 import { MemoryRouter } from 'react-router-dom';
 
